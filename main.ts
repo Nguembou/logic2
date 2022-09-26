035b1ac6bfc4bd1602cb0ce2584bed719d232eda
Nguembou <113910948+Nguembou@users.noreply.github.com>
2022-09-26 09:17:24 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    if (number == 0) {
+        basic.showIcon(IconNames.Happy)
+    } else if (number == 0) {
+        basic.showIcon(IconNames.Rollerskate)
+    } else {
+        basic.showIcon(IconNames.Rollerskate)
+    }
+})
+let number = 0
+number = randint(0, 2)
+basic.forever(function () {
+	
+})
